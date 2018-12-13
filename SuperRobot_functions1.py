3f8734365e2592f0118ea09bff3e382b28ab470b
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-13 09:10:58 +0000

    improved version of check_balance

--- SuperRobot_functions1.py
+++ SuperRobot_functions1.py
@@ -22,72 +22,69 @@ class ShoppingAssistant(object):
     
         
     def get_product(self):
-        self.product=input("Hello! You have a balance of {}.\n What do you want to buy today? \n Choose from laptop, phone or smartwatch. \n ".format(self.balance))
+        self.product=input("Hello! You have a balance of {}.\nWhat do you want to buy today?\nChoose from laptop, phone or smartwatch:\n ".format(self.balance))
         return self.product
         
         
     def buy(self):
         if self.product=="laptop":
-            print("\n A laptop costs 1020")
+            print("\nA laptop costs 1020")
             self.balance-=1020
             print("Your purchase has been successful!")
             print("Your balance is now: ",self.balance)
             return self.balance
         elif self.product=="phone":
-            print("\n A phone costs 800")
+            print("\nA phone costs 800")
             self.balance-=800
             print("Your purchase has been successful!")
             print("Your balance is now: ",self.balance)
             return self.balance
         elif self.product=="smartwatch":
-            print("\n A smartwatch costs 850")
+            print("\nA smartwatch costs 850")
             self.balance-=850
             print("Your purchase has been successful!")
             print("Your balance is now: ",self.balance)
             return self.balance
         else:
-            print("\n Sorry, we don't sell boring stuff")
+            print("\nSorry, we don't sell boring stuff")
     
    
             
     def check_balance(self):
-        """
-        if self.balance>=800:
-            print("\nYou can continue shopping!!")
-            self.product=input("Choose from laptop, phone or smartwatch:\n ")
-            #self.get_product()
-            self.buy()
-            #print(self.balance)
-            self.check_balance()
-            return self.balance
-            """
         if self.balance>=1020:
             print("\nYou can continue shopping!!")
             self.product=input("Choose from laptop, phone or smartwatch: \n ")
-            #self.get_product()
             self.buy()
-            #print(self.balance)
             self.check_balance()
             return self.balance
         elif self.balance>=850:
             print("\nYou can continue shopping!!")
             self.product=input("Choose from phone or smartwatch: \n ")
-            #self.get_product()
-            self.buy()
-            #print(self.balance)
-            self.check_balance()
-            return self.balance
+            if self.product=="phone":
+                self.buy()
+                self.check_balance()
+                return self.balance
+            elif self.product=="smartwatch":
+                self.buy()
+                self.check_balance()
+                return self.balance
+            else:
+                print("You don't have enough balance for that product!")
+                return self.balance
         elif self.balance>=800:
             print("\nYou can continue shopping!!")
             self.product=input("Your balance will be enough to buy a phone. Please type phone if you want to buy one:\n ")
-            #self.get_product()
-            self.buy()
-            #print(self.balance)
-            self.check_balance()
-            return self.balance
+            #self.product=input("Choose from phone or smartwatch: \n ")
+            if self.product=="phone":
+                self.buy()
+                self.check_balance()
+                return self.balance
+            else:
+                print("You don't have enough balance for that product!")
+                return self.balance
         
         else:
-            print("\n You don't have enough balance for shopping!You will receive a loan of 1000!!")
+            print("\nYou don't have enough balance for shopping!You will receive a loan of 1000!!")
             self.give_loan()
             self.loans_counter+=1
             self.give_loan()
@@ -101,15 +98,12 @@ class LoaningShoppingAssistant(ShoppingAssistant):
         
         if self.loans_counter==0:
             self.balance+=loan_amount
-            #print(self.balance)
-            print("\n You now have: " +str(self.balance) + "! You can continue shopping!!")
+            print("\nYou now have: " +str(self.balance) + "! You can continue shopping!!")
             self.product=input("Choose from laptop, phone or smartwatch: \n ")
             self.buy()
-            #self.check_balance()
             return self.balance
         else:
-            #print("\n You now have only ",self.balance)
-            print("\n You don't have enough balance to continue your shopping! \n You're not entitled to another loan yet.  Please pay your previous loan first!")
+            print("\nYou don't have enough balance to continue your shopping! \n You're not entitled to another loan yet.  Please pay your previous loan first!")
 
  
      

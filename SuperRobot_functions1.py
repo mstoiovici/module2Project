d6335f4dc3ac6596cd30314958055eae502287f9
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-12-13 00:45:26 +0000

    improved version of project in file -functions1

---
+++ SuperRobot_functions1.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 12:18:34 2018
+
+@author: maria
+"""
+
+class ShoppingAssistant(object): 
+    
+    def __init__(self, name,product=" ", age=0, balance=0, loans_counter=0):
+        self.name=name
+        self.product=product
+        self.age=age
+        self.balance=balance
+        self.loans_counter=loans_counter
+    
+    def does_shopping(self):
+        self.get_product()
+        self.buy()
+        self.check_balance()    
+    
+        
+    def get_product(self):
+        self.product=input("Hello! You have a balance of {}.\n What do you want to buy today? \n Choose from laptop, phone or smartwatch. \n ".format(self.balance))
+        return self.product
+        
+        
+    def buy(self):
+        if self.product=="laptop":
+            print("\n A laptop costs 1020")
+            self.balance-=1020
+            print("Your purchase has been successful!")
+            print("Your balance is now: ",self.balance)
+            return self.balance
+        elif self.product=="phone":
+            print("\n A phone costs 800")
+            self.balance-=800
+            print("Your purchase has been successful!")
+            print("Your balance is now: ",self.balance)
+            return self.balance
+        elif self.product=="smartwatch":
+            print("\n A smartwatch costs 850")
+            self.balance-=850
+            print("Your purchase has been successful!")
+            print("Your balance is now: ",self.balance)
+            return self.balance
+        else:
+            print("\n Sorry, we don't sell boring stuff")
+    
+   
+            
+    def check_balance(self):
+        if self.balance>=800:
+            print("\nYou can continue shopping!!")
+            self.product=input("Choose from laptop, phone or smartwatch ")
+            #self.get_product()
+            self.buy()
+            #print(self.balance)
+            self.check_balance()
+            return self.balance
+        else:
+            print("\n You don't have enough balance for shopping!You will receive a loan of 1000!!")
+            self.give_loan()
+            self.loans_counter+=1
+            self.give_loan()
+            return self.loans_counter
+            
+        
+    
+class LoaningShoppingAssistant(ShoppingAssistant):
+ 
+     def give_loan(self,loan_amount=1000):
+        
+        if self.loans_counter==0:
+            self.balance+=loan_amount
+            #print(self.balance)
+            print("\n You now have: " +str(self.balance) + "! You can continue shopping!!")
+            self.product=input("Choose from laptop, phone or smartwatch ")
+            self.buy()
+            #self.check_balance()
+            return self.balance
+        else:
+            #print("\n You now have only ",self.balance)
+            print("\n You don't have enough balance to continue your shopping! \n You're not entitled to another loan yet.  Please pay your previous loan first!")
+
+ 
+     
+ ##############take the check_balance to the subclass and make another main function in subclass that you'll call in the tests      
+     
+    
+        
+        
+   
--- SuperRobot_test.py
+++ SuperRobot_test.py
@@ -5,13 +5,13 @@ Created on Fri Dec  7 12:18:56 2018
 
 @author: maria
 """
-from SuperRobot_functions import *
+from SuperRobot_functions1 import *
 
-Mari=MiniBalanceTrack("Sera", 18, 2000)
+Mari=LoaningShoppingAssistant("Sera","", 18, 2000)
 
-product=input("Welcome to this shop! You have a balance of 2000. What do you want to buy today? Choose from laptop, phone or smartwatch. ")
-Mari.substract_from_balance(product)
-Mari.check_balance()
-Mari.check_balance()
-Mari.check_balance()
+
+Mari.does_shopping()
+#Mari.check_balance()
+#Mari.check_balance()
+#Mari.check_balance()
 
